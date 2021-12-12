c762ffbd1ced16f4602e8f8a63dd4e38256fdae3
Gabriel Gomes <dev.gabrielsoares221@gmail.com>
2021-12-12 18:03:11 -0300

    feat: agora é possível mover as peças pretas

--- js/main.js
+++ js/main.js
@@ -1,10 +1,46 @@
 var numeroCasas = 8*8
-function moverPeca(espacoClicado) {
-    console.log(espacoClicado)
 
+function zerarMovimento() {
+    for (let i = 0; i < numeroCasas; i++) {
+        if (document.getElementById('espaco'+i).classList.contains('localPossivel')) {
+            document.getElementById('espaco'+i).classList.remove('localPossivel')
+        }
+    }
+}
+
+function moverPeca(espacoClicado) {
+    for (let i = 0; i < numeroCasas; i++) {
+        if (document.getElementById('espaco'+i).classList.contains('localPossivel')) {
+            document.getElementById('espaco'+i).classList.remove('localPossivel')
+        }
+    }
     var divFilha = document.getElementById('espaco' + espacoClicado).children[0]
     var classeDivFilha = divFilha.classList.toString()
     if (classeDivFilha == "pecasPretas") {
-        console.log("entrou")
+
+       document.getElementById('espaco'+(espacoClicado+7)).classList.add('localPossivel');
+       document.getElementById('espaco'+(espacoClicado+7)).setAttribute('onclick','finalizarMovimentoEsquerda('+espacoClicado+')')
+       document.getElementById('espaco'+(espacoClicado+9)).classList.add('localPossivel');
+       document.getElementById('espaco'+(espacoClicado+9)).setAttribute('onclick','finalizarMovimentoDireita('+espacoClicado+')')
     }
+}
+
+function finalizarMovimentoDireita(localAnterior) {
+    document.getElementById('espaco'+localAnterior).innerHTML = localAnterior
+    var novoLocal = localAnterior + 9
+    let novaDiv = document.createElement('div')
+    novaDiv.classList.add('pecasPretas')
+    novaDiv.setAttribute('onclick', 'moverPeca('+novoLocal+")")
+    document.getElementById('espaco'+novoLocal).innerHTML = ""
+    document.getElementById('espaco'+novoLocal).appendChild(novaDiv)
+}
+
+function finalizarMovimentoEsquerda(localAnterior) {
+    document.getElementById('espaco'+localAnterior).innerHTML = localAnterior
+    var novoLocal = localAnterior + 7
+    let novaDiv = document.createElement('div')
+    novaDiv.classList.add('pecasPretas')
+    novaDiv.setAttribute('onclick', 'moverPeca('+novoLocal+")")
+    document.getElementById('espaco'+novoLocal).innerHTML = ""
+    document.getElementById('espaco'+novoLocal).appendChild(novaDiv)
 }
--- js/tabuleiro.js
+++ js/tabuleiro.js
@@ -2,7 +2,7 @@ document.getElementById('tabuleiro')
 var numeroCasas = 8*8
 function gerarTabuleiro() {
     let tabuleiro = document.getElementById('tabuleiro')
-    for (var i = 0; i < numeroCasas; i++) {
+    for (let i = 0; i < numeroCasas; i++) {
 
         if (i % 8 == 0) {
             var novaLinha = document.createElement('div')
@@ -15,7 +15,7 @@ function gerarTabuleiro() {
         novaDiv.innerHTML = `${i}`
         novaLinha.appendChild(novaDiv)
     }
-    var numeroLinha = document.getElementsByClassName('linha').length
+    // var numeroLinha = document.getElementsByClassName('linha').length
     var linha = 0
     for (var i = 0; i < numeroCasas; i++) {
         if (i % 2 != 0 && linha % 2 == 0) {
@@ -33,11 +33,11 @@ function gerarTabuleiro() {
 
 function gerarPecas() {
     var linha = 0
-    var novaDiv
+    let novaDiv
     for (var i = 0; i < numeroCasas; i++) {
         if (linha <= 2) {
             if (i % 2 != 0 && linha % 2 == 0) {
-                novaDiv = document.createElement('div')
+                let novaDiv = document.createElement('div')
                 novaDiv.classList.add('pecasPretas')
                 novaDiv.setAttribute('onclick', 'moverPeca('+i+")")
                 document.getElementById('espaco' + i).innerHTML = ""
@@ -53,7 +53,7 @@ function gerarPecas() {
         }
         if (linha >= 5) {
             if (i % 2 != 0 && linha % 2 == 0) {
-                novaDiv = document.createElement('div')
+                let novaDiv = document.createElement('div')
                 novaDiv.classList.add('pecasBrancas')
                 novaDiv.setAttribute('onclick', 'moverPeca('+i+")")
                 document.getElementById('espaco' + i).innerHTML = ""
